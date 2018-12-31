4f455ad5e6407c4eca1b6a74750173bee68f96d9
alkanpolat <alkan_polat_41@hotmail.com>
2018-12-31 20:18:57 +0300

    Update reducer.js

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,24 +7,24 @@ function reducer(
 ) {
   switch (action.type) {
 
-    case "SET_TEXT":
-    store.currentText = action.data;
-    return store;
+	case "SET_TEXT":
+   	store.currentText = action.data;
+	return store;
 
-    case "ADD_TODO":
-    store.todoItems.push(action.data);
-    return store;
-	
+	case "ADD_TODO":
+	store.todoItems.push(action.data);
+	return store;
+
 	case "REMOVE_TODO":
 	store.todoItems = store.todoItems.filter((item,i) => i!=action.data);
 	return store;
-	
+
 	case "SEARCH_TODO":
 	store.todoItems = store.todoItems.filter((item,i) => item.includes(action.data));
 	return store;
 
-    default:
-      return store;
+   default:
+	return store;
   }
 }
 
